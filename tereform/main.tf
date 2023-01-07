415188812e4919dc7373608303848874e4364fe0
devopsmvc <devops.mvc@gmail.com>
2023-01-07 10:22:53 +0530

    adding terraform assignment

--- tereform/main.tf
+++ tereform/main.tf
@@ -1,33 +1,34 @@
+provider "aws" {
+  region = "ap-south-1"
+}
 
-
-
-resource "aws_vpc" "example" {
+resource "aws_vpc" "terraform" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_internet_gateway" "example" {
-  vpc_id = aws_vpc.example.id
+resource "aws_internet_gateway" "terraform" {
+  vpc_id = aws_vpc.terraform.id
 }
 
-resource "aws_route_table" "example" {
-  vpc_id = aws_vpc.example.id
+resource "aws_route_table" "terraform" {
+  vpc_id = aws_vpc.terraform.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.example.id
+    gateway_id = aws_internet_gateway.terraform.id
   }
 }
 
-resource "aws_subnet" "example" {
-  vpc_id     = aws_vpc.example.id
+resource "aws_subnet" "terraform" {
+  vpc_id     = aws_vpc.terraform.id
   cidr_block = "10.0.1.0/24"
 
-  route_table_id = aws_route_table.example.id
+  route_table_id = aws_route_table.terraform.id
 }
 
-resource "aws_security_group" "example" {
-  name   = "example"
-  vpc_id = aws_vpc.example.id
+resource "aws_security_group" "terraform" {
+  name   = "terraform"
+  vpc_id = aws_vpc.terraform.id
 
   ingress {
     from_port   = 22
@@ -51,28 +52,28 @@ resource "aws_security_group" "example" {
   }
 }
 
-resource "aws_network_interface" "example" {
-  subnet_id = aws_subnet.example.id
+resource "aws_network_interface" "terraform" {
+  subnet_id = aws_subnet.terraform.id
 }
 
-resource "aws_eip" "example" {
+resource "aws_eip" "terraform" {
   vpc      = true
-  network_interface = aws_network_interface.example.id
+  network_interface = aws_network_interface.terraform.id
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-xxxxxxxx"
+resource "aws_instance" "terraform" {
+  ami           = "ami-07ffb2f4d65357b42"
   instance_type = "t2.micro"
   network_interface {
     device_index = 0
-    network_interface_id = aws_network_interface.example.id
+    network_interface_id = aws_network_interface.terraform.id
   }
 
-  connection {
+  /* connection {
     type        = "ssh"
     user        = "ubuntu"
     private_key = file("~/.ssh/id_rsa")
-  }
+  } */
 
   provisioner "remote-exec" {
     inline = [
